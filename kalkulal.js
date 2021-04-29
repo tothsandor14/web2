f48f001843e9a19d5544dd4ea2ba8af751a3e7c5
Tóth Sándor <pemge.sanci@gmail.com>
2021-04-29 09:01:15 +0200

    first

---
+++ kalkulal.js
@@ -0,0 +1,12 @@
+function szamol() {
+    var letszam = 1;
+    var ejszaka = 1;
+    var orszag = 'b';
+    var fizetendo;
+
+    //--számolás--
+      
+    //------------
+    document.getElementById('eredmeny').value = fizetendo+" Ft";
+    
+}
